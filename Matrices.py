ddf1a34956e5ef152aa386abe3b139838624842f
Casas005 <143463875+Casas005@users.noreply.github.com>
2023-09-08 13:51:03 -0600

    Matrices
    tarea y pueba de matrices

---
+++ Matrices.py
@@ -0,0 +1,25 @@
+# acceder a '5'
+matriz = [[1, 2, 3], 
+          [4, 5, 6], 
+          [7, 8, 9]]
+
+print(matriz[1][1])
+#-----------------------------------
+#Cambiar valor
+
+datos = [[10, 20, 30], 
+         [40, 50, 60], 
+         [70, 80, 90]]
+
+datos[1][1] = 55
+
+#-----------------------------------
+# Agregar dato
+
+estudiantes = [["Juan", 15], 
+               ["María", 16], 
+               ["Carlos", 14]]
+
+estudiantes.insert(0,["Ana, 17"])
+
+print(estudiantes)
